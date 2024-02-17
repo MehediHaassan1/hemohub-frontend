f6440723d368f1fb98b1ceb4ca1c81bf65e30ae5
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-02-17 07:14:09 +0600

    register form done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,29 @@
 import { Link } from "react-router-dom";
 import GoogleLogin from "../shared/GoogleLogin/GoogleLogin";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const onSubmit = (data) => {
+        const password = data.password;
+        const c_password = data.c_password;
+        if (password !== c_password) {
+            alert("password did not matched");
+            return;
+        }
+        console.log(data);
+    };
+
     return (
         <div className="max-w-screen-xl mx-auto">
             <div className="container mx-auto">
@@ -19,7 +41,10 @@ const Register = () => {
                             <h3 className="pt-4 text-2xl text-center text-accent">
                                 Create an Account!
                             </h3>
-                            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                            <form
+                                onSubmit={handleSubmit(onSubmit)}
+                                className="px-8 pt-6 pb-8 mb-4 bg-white rounded"
+                            >
                                 <div className="mb-4 md:flex md:justify-between">
                                     <div className="mb-4 md:mr-2 md:mb-0">
                                         <label
@@ -33,7 +58,22 @@ const Register = () => {
                                             id="firstName"
                                             type="text"
                                             placeholder="First Name"
+                                            name="firstName"
+                                            {...register("firstName", {
+                                                required: {
+                                                    value: true,
+                                                    message:
+                                                        "First Name is required",
+                                                },
+                                            })}
                                         />
+                                        <label className="block my-2 text-sm font-bold text-red-500">
+                                            {errors.firstName && (
+                                                <p>
+                                                    {errors.firstName.message}
+                                                </p>
+                                            )}
+                                        </label>
                                     </div>
                                     <div className="md:ml-2">
                                         <label
@@ -47,7 +87,20 @@ const Register = () => {
                                             id="lastName"
                                             type="text"
                                             placeholder="Last Name"
+                                            name="lastName"
+                                            {...register("lastName", {
+                                                required: {
+                                                    value: true,
+                                                    message:
+                                                        "Last Name is required",
+                                                },
+                                            })}
                                         />
+                                        <label className="block my-2 text-sm font-bold text-red-500">
+                                            {errors.lastName && (
+                                                <p>{errors.lastName.message}</p>
+                                            )}
+                                        </label>
                                     </div>
                                 </div>
                                 <div className="mb-4">
@@ -62,10 +115,27 @@ const Register = () => {
                                         id="email"
                                         type="email"
                                         placeholder="Email"
+                                        name="email"
+                                        {...register("email", {
+                                            required: {
+                                                value: true,
+                                                message: "Email is required",
+                                            },
+                                            pattern: {
+                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                                message:
+                                                    "Invalid email address",
+                                            },
+                                        })}
                                     />
+                                    <label className="block my-2 text-sm font-bold text-red-500">
+                                        {errors.email && (
+                                            <p>{errors.email.message}</p>
+                                        )}
+                                    </label>
                                 </div>
                                 <div className="mb-4 md:flex md:justify-between">
-                                    <div className="mb-4 md:mr-2 md:mb-0">
+                                    <div className="mb-2 md:mr-2 md:mb-0">
                                         <label
                                             className="block mb-2 text-sm font-bold text-gray-700"
                                             htmlFor="password"
@@ -73,14 +143,60 @@ const Register = () => {
                                             Password
                                         </label>
                                         <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border  rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                             id="password"
-                                            type="password"
+                                            type={
+                                                showPassword
+                                                    ? "text"
+                                                    : "password"
+                                            }
                                             placeholder="******************"
+                                            name="password"
+                                            {...register("password", {
+                                                required: {
+                                                    value: true,
+                                                    message:
+                                                        "Password is required",
+                                                },
+                                                minLength: {
+                                                    value: 6,
+                                                    message:
+                                                        "Must contain 6 characters",
+                                                },
+                                                validate: {
+                                                    upperCase: (fieldValue) => {
+                                                        return (
+                                                            /[A-Z]/.test(
+                                                                fieldValue
+                                                            ) ||
+                                                            "Add 1 uppercase"
+                                                        );
+                                                    },
+                                                    lowerCase: (fieldValue) => {
+                                                        return (
+                                                            /[a-z]/.test(
+                                                                fieldValue
+                                                            ) ||
+                                                            "Add 1 lowercase"
+                                                        );
+                                                    },
+                                                    numberCase: (
+                                                        fieldValue
+                                                    ) => {
+                                                        return (
+                                                            /[0-9]/.test(
+                                                                fieldValue
+                                                            ) || "Add 1 number"
+                                                        );
+                                                    },
+                                                },
+                                            })}
                                         />
-                                        <p className="text-xs italic text-red-500">
-                                            Please choose a password.
-                                        </p>
+                                        <label className="block my-2 text-sm font-bold text-red-500">
+                                            {errors.password && (
+                                                <p>{errors.password.message}</p>
+                                            )}
+                                        </label>
                                     </div>
                                     <div className="md:ml-2">
                                         <label
@@ -90,17 +206,86 @@ const Register = () => {
                                             Confirm Password
                                         </label>
                                         <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline`}
                                             id="c_password"
-                                            type="password"
+                                            type={
+                                                showPassword
+                                                    ? "text"
+                                                    : "password"
+                                            }
                                             placeholder="******************"
+                                            name="c_password"
+                                            {...register("c_password", {
+                                                required: {
+                                                    value: true,
+                                                    message:
+                                                        "Confirm Password is required",
+                                                },
+                                                minLength: {
+                                                    value: 6,
+                                                    message:
+                                                        "Must contain 6 characters",
+                                                },
+                                                validate: {
+                                                    upperCase: (fieldValue) => {
+                                                        return (
+                                                            /[A-Z]/.test(
+                                                                fieldValue
+                                                            ) ||
+                                                            "Add 1 uppercase"
+                                                        );
+                                                    },
+                                                    lowerCase: (fieldValue) => {
+                                                        return (
+                                                            /[a-z]/.test(
+                                                                fieldValue
+                                                            ) ||
+                                                            "Add 1 lowercase"
+                                                        );
+                                                    },
+                                                    numberCase: (
+                                                        fieldValue
+                                                    ) => {
+                                                        return (
+                                                            /[0-9]/.test(
+                                                                fieldValue
+                                                            ) || "Add 1 number"
+                                                        );
+                                                    },
+                                                },
+                                            })}
                                         />
+                                        <label className="block my-2 text-sm font-bold text-red-500">
+                                            {errors.c_password && (
+                                                <p>
+                                                    {errors.c_password.message}
+                                                </p>
+                                            )}
+                                        </label>
                                     </div>
                                 </div>
+                                <p
+                                    className="flex items-center gap-2 cursor-pointer w-fit mb-6"
+                                    onClick={handleShowPassword}
+                                >
+                                    <input
+                                        id="showPassword"
+                                        type="checkbox"
+                                        checked={showPassword ? true : false}
+                                        className="checkbox checkbox-xs "
+                                        readOnly
+                                    />
+                                    <span
+                                        htmlFor="showPassword"
+                                        className="block my-2 text-sm font-bold text-primary"
+                                    >
+                                        Show Password
+                                    </span>
+                                </p>
                                 <div className="mb-6 text-center">
                                     <button
                                         className="w-full px-4 py-2 font-bold text-white bg-primary rounded focus:outline-none focus:shadow-outline"
-                                        type="button"
+                                        type="submit"
                                     >
                                         Register Account
                                     </button>
